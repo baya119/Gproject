2f7f5a06eb2ccbf969f8ed59817a0861a5acdd00
behailu adugna <behailuadugn0@gmail.com>
2023-05-02 09:49:47 +0300

    second

---
+++ frontend/src/componets/MenuData.js
@@ -0,0 +1,36 @@
+export const MenuData =[
+    {
+        title: "Home",
+        url: "/",
+        cName: "nav-links",
+        icon: "",
+    },
+    {
+        title: "Bids",
+        url: "/bids",
+        cName: "nav-links",
+        icon: "",
+    },
+    {
+        title: "About",
+        url: "/about",
+        cName: "nav-links",
+        icon: "",
+    },
+    {
+        title: "Contact",
+        url: "/contact",
+        cName: "nav-links",
+        icon: "",
+    },
+    {
+        title: "log in",
+        url: "#",
+        cName: "nav-links-mobile",
+    },
+    {
+        title: "sign up",
+        url: "#",
+        cName: "nav-links-mobile",
+    },
+]
