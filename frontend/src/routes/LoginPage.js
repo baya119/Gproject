d9fef61c9130786a81c44e68360240f51e96752e
Ermias Gashaw <ermiasgashaw7881@gmail.com>
2023-06-04 17:19:21 +0300

    login page

--- frontend/src/routes/LoginPage.js
+++ frontend/src/routes/LoginPage.js
@@ -1,15 +1,46 @@
-import React from 'react'
+import React, {useState} from 'react'
 import { Link } from 'react-router-dom'
 import "../componets/Login.css";
 
-
-import '../../src/App'
+import '../../src/App';
+import api from '../api';
 
 export default function SignInPage() {
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+      
+        // Perform form validation
+        if (!email || !password) {
+          // Handle validation errors
+          return;
+        }
+      
+        // Make an API request to register the user
+        api.post('/login/', {
+            email: email,
+            password: password,
+        })
+          .then((response) => {
+            const token = response.data.token;
+            localStorage.setItem('authToken', token);
+            window.location.href = '/bids';
+          })
+          .catch(error => {
+            if (error.response && error.response.status === 400) {
+              const errors = error.response.data;
+              console.log(errors);
+            } else {
+              // Handle other non-validation errors
+            }
+          });
+      };
     return (
         <div className="text-center m-5-auto">
             <h2>Sign in to us</h2>
-            <form action="/home">
+            <form onSubmit={handleSubmit}>
                 <p>
                     <label>Username or Email Address</label><br/>
                     <input type="text" name="first_name" required />
