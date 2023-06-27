e1000b09eac8f4f8fb560daf23969f5734062bed
Amanuel Abebayehu <64314295+aman-abeb@users.noreply.github.com>
2023-06-27 12:14:05 +0300

    Delete backend directory

--- backend/README.md
+++
@@ -1,2 +0,0 @@
-# Biding-and-Merchandizing-Platform
-backend application for senior project
--- backend/payment/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path, re_path
-from . import views
-
-urlpatterns = [
-    path('deposit/', views.Deposit.as_view(), name="deposit"),
-    path('buy-document/', views.Buy.as_view(), name="buy document"),
-    path('cpo/', views.Cpo.as_view(), name="cpo"),
-    path('payment-success/', views.PaymentSuccess.as_view())
-]
